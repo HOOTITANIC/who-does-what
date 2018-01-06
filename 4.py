d46f08b6fa4abb1dc3b9b540fb251cf7ef34efa8
HOOTITANIC <34877100+HOOTITANIC@users.noreply.github.com>
2018-01-06 07:40:54 -0600

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,11 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> what_he_does='plays'
+>>> his_instrument='guitar'
+>>> his_name='Robert Johnson'
+>>> artist_intro=his_name+what_he_does+his_instrument
+>>> 
+>>> print(artist_intro)
+Robert Johnsonplaysguitar
+>>> 
+>>> 
